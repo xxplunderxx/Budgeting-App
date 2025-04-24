f55287e89512a4ddbdf779cd94bb00f27b309eda
xxplunderxx <20jeul@gmail.com>
2025-04-24 17:03:53 -0500

    save progress

--- Login-UI/Login-UI/node_modules/@react-native-google-signin/google-signin/lib/typescript/src/constants.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const cancelledResult: Readonly<{
-    type: "cancelled";
-    data: null;
-}>;
-export declare const noSavedCredentialFoundResult: Readonly<{
-    type: "noSavedCredentialFound";
-    data: null;
-}>;
-//# sourceMappingURL=constants.d.ts.map
