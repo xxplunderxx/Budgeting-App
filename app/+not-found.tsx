9ab17381e32c4c3e047ffe3575fb4be83863ddc4
xxplunderxx <20jeul@gmail.com>
2025-04-24 17:08:30 -0500

    fixed repo

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,9 +1,6 @@
 import { Link, Redirect, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
 export default function NotFoundScreen() {
   return <Redirect href="/" />;
 }
